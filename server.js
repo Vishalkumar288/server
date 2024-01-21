8b0def048a73ada0c1e808977ad2db06983cf39a
VISHAL KUMAR <90972522+Vishalkumar288@users.noreply.github.com>
2024-01-21 14:39:55 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://MyFirstDB:MomNDad_1010@mycluster.50e4enm.mongodb.net/",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const Note = mongoose.model("Note", {
+  dateTime: String,
+  note: String,
+});
+
+app.post("/notes/create", async (req, res) => {
+  const { note, dateTime } = req.body;
+  // console.log()
+
+  if (!note || !dateTime) {
+    return res
+      .status(400)
+      .json({ error: "dateTime and note are required in the request body" });
+  }
+
+  // Create a new note using the Mongoose model
+  const newNote = new Note({ dateTime, note });
+
+  try {
+    // Save the note to the database
+    await newNote.save();
+    res.json({ message: "Note created" });
+  } catch (error) {
+    console.error("Error creating note:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/notes", async (req, res) => {
+  try {
+    // Fetch all notes from the database
+    const notes = await Note.find();
+    res.json(notes);
+  } catch (error) {
+    console.error("Error getting notes:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server Started at port 5000");
+});
